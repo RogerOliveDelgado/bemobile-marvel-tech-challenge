26435c0e4083c5b9aefd00c8cfb9c4b891830e40
r.olive <r.olive@plugchain.energy>
2024-12-05 13:24:12 +0100

    refactor: Use GridLayout reusable component on Favorites Page

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,67 +1,42 @@
 /* eslint-disable no-unused-vars */
-import heartSelected from '@/assets/images/heart-selected.svg'
-import heartUnselected from '@/assets/images/heart-unselected.svg'
+import Card from '@/components/Card/Card'
+import GridLayout from '@/components/Grid/GridLayout/GridLayout'
 import { useFavorites } from '@/contexts/FavoritesContext'
-import type { Character } from '@/pages/Home/Home'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import type { Character } from '../Home/Home'
 import styles from './Favorites.module.css'
 
 const Favorites: React.FC = () => {
   const navigate = useNavigate()
-  const { favorites } = useFavorites()
+  const { favorites, toggleFavorite, isFavorite } = useFavorites()
 
   const handleNavigate = (character: Character) => {
     navigate(`/character/${character.id}`)
   }
-  const { toggleFavorite, isFavorite } = useFavorites()
-
-  const handleFavorite = (character: Character) => () => {
-    toggleFavorite(character)
-  }
 
   return (
-    <div className={styles.favorites}>
-      <h1 className={styles.title}>Favorites</h1>
-
+    <>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Favorites</h1>
+      </div>
       {favorites.length === 0 ? (
         <p className={styles.emptyMessage}>No favorite characters yet.</p>
       ) : (
-        <div className={styles.dashboard}>
-          {favorites.map((character) => (
-            <div key={character.id} className={styles.characterCard}>
-              <div
-                className={styles.characterImageWrapper}
-                onClick={() => handleNavigate(character)}
-              >
-                <img
-                  src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                  alt={`${character.name} Thumbnail`}
-                  className={styles.characterImage}
-                />
-              </div>
-              <div className={styles.characterCardDivider}></div>
-              <div className={styles.characterInfo}>
-                <p className={styles.characterName}>{character.name}</p>
-                <button
-                  className={styles.favoriteButton}
-                  aria-label="Toggle from favorites"
-                  onClick={handleFavorite(character)}
-                >
-                  <img
-                    src={
-                      isFavorite(character.id) ? heartSelected : heartUnselected
-                    }
-                    alt="Toggle Favorites"
-                    className={styles.heartIcon}
-                  />
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <GridLayout
+          items={favorites}
+          renderItem={(character: Character) => (
+            <Card
+              key={character.id}
+              character={character}
+              onNavigate={handleNavigate}
+              onAddFavorite={toggleFavorite}
+              isFavorite={isFavorite}
+            />
+          )}
+        />
       )}
-    </div>
+    </>
   )
 }
 
