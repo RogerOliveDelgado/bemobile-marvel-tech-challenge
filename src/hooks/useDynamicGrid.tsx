080a00ac8458b118629d8a674f4aab8d1d40bda0
r.olive <r.olive@plugchain.energy>
2024-12-11 21:52:25 +0100

    Fix: Delete useDebounce from useDynamicGrid - don't needed

--- src/hooks/useDynamicGrid.tsx
+++ src/hooks/useDynamicGrid.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import useDebounce from './useDebounce'
 
 function useDynamicGrid(
   containerRef: React.RefObject<HTMLDivElement>,
