83e7eaee685fcfe82c00643f7398ead2e67f93f8
r.olive <r.olive@plugchain.energy>
2024-12-12 09:45:09 +0100

    Remove MainLayout - no needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import MainLayout from '@/layouts/MainLayout'
 import AppRoutes from '@/routes/AppRoutes'
 import React from 'react'
 
 const App: React.FC = () => {
-  return (
-    <MainLayout>
-      <AppRoutes />
-    </MainLayout>
-  )
+  return <AppRoutes />
 }
 
 export default App
